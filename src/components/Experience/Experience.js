fb288815058306a7066646ccc1c5be8b4fe5a7ee
John Schmitt <jjshutout19@yahoo.com>
2025-02-06 18:02:41 -0600

    added basic layout and css to experience page. Will work more tomorrow on that page. Going to go have fun with my friends now! Happy thursday

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,11 +1,28 @@
 import React from "react";
 import MyNavBar from "../Navigation/MyNavBar";
+import './experience.css';
 
 function Experience() {
     return (
-        <div>
+        <div className="experience-wrapper">
             <MyNavBar />
-            <p>Experience Page</p>
+            <div className="container">
+                <div className="coursework-section">
+                    <div className="left col">
+                        {/* Have a list of all my semester of schooling.
+                            when one is selected, show the classes in right col */}
+                        <p>coursework-left-col</p>
+                    </div>
+                    <div className="right col">
+                        {/* Grid structure (can change) to show coursework from selected semester */}
+                        <p>coursework-right-col</p>
+                    </div>
+                </div>
+                <div className="resume-section">
+                    {/* Have resume information here, as well as a button to download my resume */}
+                    <p>resume-section</p>
+                </div>
+            </div>
         </div>
     )
 }
