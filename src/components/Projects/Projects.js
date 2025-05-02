b698ee1932209d0d961bd81bbed3646b38fce671
John Schmitt <jjshutout19@yahoo.com>
2025-05-02 18:12:18 -0500

    added etl_pipeline project to projects page. Will go back soon and change about me paragraph

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,11 +8,13 @@ function Projects() {
     const descriptions = {
         "Arbet": "Developed an automated arbitrage detection system that retrieves real-time betting lines for select sports via an API. The system processes data in Node.js, transforming and forwarding it to a Rust-based computation engine that efficiently analyzes arbitrage opportunities across multiple markets. The results are returned to the Node.js environment via JSON for further processing or display.",
         "Replicator": "I developed a replicator program in Rust to streamline and enhance our team's testing capabilities. This tool enables engineers to efficiently bootstrap from a heterogeneous machine cluster to an experimental service cell with a single command, significantly reducing setup time and improving deployment consistency. By automating this process, the program enhances scalability and ensures a seamless transition between environments, ultimately optimizing the team's workflow for future testing methods.",
-        "Personal Portfolio": "Developed this very website through a combination of React.js and CSS. You can access the code base on my GitHub under 'portfolio-project'."
+        "Personal Portfolio": "Developed this very website through a combination of React.js and CSS. You can access the code base on my GitHub under 'portfolio-project'.",
+        "S&P 100 ETL Pipeline": "Built an ETL pipeline that webscrapes and makes API calls to get financial data for S&P 100 companies. The data is then transformed to provide various metrics and is stored in a local duckdb database for efficient querying and a local sqlite db for backup. Both databases are updated every weekday by a script that runs on a cron job and gets the data for that trading day. All updates and errors are logged to a log file."
     }
 
     /* TODO: Change Arbet src to something different */
     const myProjects = [
+        {name: "S&P 100 ETL Pipeline", src: "/content/etl_pipeline.png", description: descriptions["S&P 100 ETL Pipeline"]},
         {name: "Arbet", src: "/content/replicator.png", description: descriptions["Arbet"]},
         {name: "Replicator", src: "/content/replicator.png", description: descriptions["Replicator"]},
         {name: "Personal Portfolio", src: "/content/personal_portfolio.png", description: descriptions["Personal Portfolio"]}
