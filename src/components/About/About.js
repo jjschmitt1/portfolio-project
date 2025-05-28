8032455b3ef1c2c53eb78d022775790fbab27063
John Schmitt <jjshutout19@yahoo.com>
2025-05-28 10:13:55 -0500

    updated about page as well as new version of resume

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -36,14 +36,22 @@ function About() {
                     <div className="left-column">
                         <h1>About Me</h1>
                         <p>
-                        Hello! My name is JJ Schmitt, and I am a December 2024 graduate of UW-Madison. I enjoy playing golf, 
+                        {/*Hello! My name is JJ Schmitt, and I am a December 2024 graduate of UW-Madison. I enjoy playing golf, 
                         cooking, lifting weights, spending time with family and friends, and both playing and watching soccer. 
                         I’m also a passionate fan of Chicago and UW-Madison sports.
 
                         My journey into programming began in 5th grade, when a traveling coding class introduced my elementary 
                         school to drag-and-drop coding. From there, I was fortunate to attend a middle and high school that 
                         offered a wide range of programming courses, allowing me to develop and refine my skills over the years. 
-                        I've been coding ever since!
+                        I've been coding ever since!*/}
+                        Hello! My name is JJ Schmitt and I recently graduated from UW-Madison with a B.S. in Computer Science and
+                        minors in Math and German. Since graduation, I've spent time learning new programming languages, attempting 
+                        to get into shape, and building projects that I either find interesting or align with my future career goals.
+                        My current career goal is to earn a position as an entry level data engineer. The combination of coding and
+                        manipulating numbers is something that I feel innately drawn towards. Outside of my professional life, I love
+                        to play golf, lift weights, cook protein-heavy meals, watch sports, and hang with my friends. Thanks for checking
+                        out my website!
+
                         </p>
                         {/* TODO: Add a "quote of the week" thing, where I can put cool quotes I like
                             quote 1: An idiot admires complexity, a genius admires simplicity - Terry A. Davis*/}
